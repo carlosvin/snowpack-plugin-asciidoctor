9179addf855890ebe18c7cfadbaffc63ddd3c739
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-12-06 19:19:52 +0100

    refactor: tests reading 2 different posts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,24 +2,18 @@ import path from 'path'
 import { promises as fs } from 'fs'
 const snowpackPluginAdoc = require('../index.ts')
 
+Date.now = jest.fn(() => 1482363367071)
+
 describe('snowpack-plugin-asciidoctor', () => {
   it('should compile .adoc files', async () => {
     const filePath = path.join(__dirname, '__fixtures__/test.adoc')
-    const contents = await fs.readFile(filePath, 'utf-8')
     const plugin = snowpackPluginAdoc({}, {})
-    const result = await plugin.load({ contents, filePath })
-    expect(result.html).toMatchSnapshot('html')
-    /* TODO remove those properties
-    expect(result.meta).toMatchSnapshot({
-      localdate: expect.any(String),
-      localdatetime: expect.any(String),
-      localtime: expect.any(String),
-      localyear: expect.any(String),
-    })*/
+    const result = await plugin.load({ filePath })
+    expect(result).toMatchSnapshot()
   })
 
   it('should include files', async () => {
-    const filePath = path.join(__dirname, '__fixtures__/test.adoc')
+    const filePath = path.join(__dirname, '__fixtures__/a-post.adoc')
     const contents = await fs.readFile(filePath, 'utf-8')
     const plugin = snowpackPluginAdoc(
       {},
@@ -29,6 +23,6 @@ describe('snowpack-plugin-asciidoctor', () => {
     )
     const result = await plugin.load({ contents, filePath })
     expect(result).not.toBeNull()
-    expect(result.html).toMatchSnapshot('include')
+    expect(result).toMatchSnapshot()
   })
 })
